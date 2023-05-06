c6fa961d4ec4b437ea96e0fda488e8d959982539
luis <luis@pop-os.localdomain>
2023-05-06 00:03:28 -0300

    001

--- configuracao.php
+++ configuracao.php
@@ -1,1 +1,1 @@
-configuracao
+configuração
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+echo '<h1>Arquivo Index</h1>';
+echo "<h2>Texto dentro de aspas duplas</h2>";
+
+print "<p>Texto usando print</p>";
+print "<small>Texto dentro de aspas duplas</small>";
+// Comentario de uma linha
+
+# comentario de uma linha
+
+/*
+multiplas linhas
+¯`v´¯) I Love My life
+`*.¸.*´ ?
+¸.•´¸.•*¨) ¸.•*¨)?
+(¸.•´ (¸.•´ .•´ ¸¸.•¨¯`•.
+(¯`v´¯)
+.`·.¸.·´ ?
+¸.·´¸.·´¨) ¸.·*¨)
+(¸.·´ (¸.·´ .·´ ¸
+
+*/
+
+?>
